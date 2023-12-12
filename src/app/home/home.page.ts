3cd03995e58437cb334b8b1bbe16a0c5c6246a5c
unknown <jcorvil600@g.educaand.es>
2023-12-12 13:56:47 +0100

    .

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,13 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  users: any;
+  filtro: string = "";
+
+  constructor(private httpClient:HttpClient) {
+
+    this.users = this.httpClient.get('https://raw.githubusercontent.com/Jcorvil/arrayAgenda/master/array.json').pipe(map((res:any)=>res['results']));
+
+  }
 
 }
