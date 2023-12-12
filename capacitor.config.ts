7bdcdf0e4cdcd5e8eb0d1a7a3d70d984d2de68c5
unknown <jcorvil600@g.educaand.es>
2023-12-12 11:16:10 +0100

    12/12/2023. Se rehace el proyecto

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: '1.14AppResponsivePersonalizada',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
