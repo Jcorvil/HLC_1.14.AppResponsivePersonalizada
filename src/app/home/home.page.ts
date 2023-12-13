55251534b1ffef5c6739b89fd79779f88dc7f171
unknown <jcorvil600@g.educaand.es>
2023-12-13 09:56:26 +0100

    13/12/2023. Se termina proyecto base. Faltan estilos

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {map} from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -11,11 +11,31 @@ export class HomePage {
 
   animals: any;
   filtro: string = "";
+  filtroAnimales: any;
+  tipoFiltro: string = "nombreComun";
 
-  constructor(private httpClient:HttpClient) {
-
+  constructor(private httpClient: HttpClient) {
     this.animals = this.httpClient.get('https://raw.githubusercontent.com/Jcorvil/HLC_1.14.AppResponsivePersonalizada/master/arrayanimales/animals.json');
 
+
+    this.filtroAnimales = this.animals;
+  }
+
+  cumpleFiltro(animal: any): boolean {
+    if (!this.filtro) return true;
+    
+    const filtroLower = this.filtro.toLowerCase();
+
+    switch (this.tipoFiltro) {
+      case 'nombreComun':
+        return animal?.name?.common.toLowerCase().startsWith(filtroLower);
+      case 'nombreCientifico':
+        return animal?.name?.scientific.toLowerCase().startsWith(filtroLower);
+      case 'pais':
+        return animal?.country.toLowerCase().startsWith(filtroLower);
+      default:
+        return true;
+    }
   }
 
 }
